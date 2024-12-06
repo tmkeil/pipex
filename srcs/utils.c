a3d5870784933880f1872c8d0af4d6d5d9658ded
Tobias Keil <tkeil@student.42heilbronn.de>
2024-12-06 21:50:39 +0100

    basic pipe functions

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,38 +6,20 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 13:43:03 by tkeil             #+#    #+#             */
-/*   Updated: 2024/12/06 20:38:48 by tkeil            ###   ########.fr       */
+<<<<<<< HEAD
+/*   Updated: 2024/12/07 19:55:49 by tkeil            ###   ########.fr       */
+=======
+/*   Updated: 2024/12/06 22:45:17 by tkeil            ###   ########.fr       */
+>>>>>>> 2b565b7 (almost all)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-long	ft_atol(char *s)
+void	ft_error(int errno, int fd)
 {
-	size_t	i;
-	int		p;
-	long	val;
-	bool	valid;
-
-	i = 0;
-	p = 1;
-	val = 0;
-	valid = false;
-	while (s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
-		i++;
-	if (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i++] == '-')
-			p = -1;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		valid = true;
-		val = val * 10 + (s[i++] - '0');
-	}
-	if (!valid || s[i] != '\0')
-		return (LONG_MIN);
-	return (val * p);
+	ft_putendl_fd(strerror(errno), fd);
+	exit(errno);
 }
 
 void	ft_clr(char **ptr)
@@ -53,31 +35,43 @@ void	ft_clr(char **ptr)
 	ptr = NULL;
 }
 
-char	*ft_getpath(char *cmd, char **envp)
+static char	*ft_check_paths(char *env, char *cmd)
 {
+	int		i;
 	char	*path;
 	char	*full;
-	char	**env;
-	char	**start;
 
+	i = 0;
 	path = NULL;
 	full = NULL;
-	while (*envp && !ft_strnstr(*envp, "PATH=", 5))
-		envp++;
-	env = ft_split(*envp, ':');
-	start = env;
-	if (!env || !*env)
-		return (NULL);
-	while (*env)
-	{
-		path = ft_strjoin(*env++, "/");
-		full = ft_strjoin(path, cmd);
-		free(path);
+	while (env[i])
+    {
+        path = ft_strjoin(env[i], "/");
+        full = ft_strjoin(path, cmd);
+        free(path);
         if (!full)
-            return (ft_clr(start), NULL);
-		if (access(full, F_OK) == 0)
-			return (ft_clr(start), full);
+            return (ft_clr(&env), NULL);
+        if (access(full, X_OK) == 0)
+			return (ft_clr(&env), full);
 		free(full);
-	}
-	return (ft_clr(start), NULL);
+		i++;
+    }
+    return (ft_clr(&env), NULL);
+}
+
+char    *ft_getpath(char *cmd, char **envp)
+{
+    char    **env;
+
+    if (!envp || !*envp)
+        return (NULL);
+    while (*envp)
+    {
+        if (ft_strnstr(envp++, "PATH=", 5))
+            break ;
+    }
+    env = ft_split(envp + 5, ' ');
+    if (!env || !*env)
+        return (NULL);
+	return (ft_check_paths(env, cmd));
 }
